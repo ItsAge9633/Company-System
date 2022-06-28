b9257432103d668ae3e8084628473e6c6e467434
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-06-29 02:30:44 +0530

    MCQ Examination Integration: 60% Done

---
+++ MCQ/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Quiz Platform</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <style>
+        .bodycolor{
+            background: #9053c7;
+            background: -webkit-linear-gradient(-135deg, #c850c0, #4158d0);
+            background: -o-linear-gradient(-135deg, #c850c0, #4158d0);
+            background: -moz-linear-gradient(-135deg, #c850c0, #4158d0);
+            background: linear-gradient(-135deg, #c850c0, #4158d0);
+            height: 100%;
+            margin: 0;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md" style="color: var(--indigo);background: #242226;">
+        <div class="container-fluid"><a class="navbar-brand" href="index.html" style="color:aliceblue">MCQ Software</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active" href="index.php" style="color:aliceblue">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php" style="color:aliceblue">Admin Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="squizregister.php" style="color:aliceblue">Enter Quiz</a></li>
+                    <li class="nav-item"><a class="nav-link" href="result.php" style="color:aliceblue">Results</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" style="color:aliceblue">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#" style="color:aliceblue">Contact Us</a></li>     
+                </ul>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav ">
+                    <img src="assets/img/my_logo.jpeg" alt="" width="70" height="70">			
+                </ul>		  
+                </div>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <div class="container jumbotron">
+        <h1>MCQ Software</h1>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+      <!-- Vendor JS Files -->
+    <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/chart.js/chart.min.js"></script>
+    <script src="assets/vendor/echarts/echarts.min.js"></script>
+    <script src="assets/vendor/quill/quill.min.js"></script>
+    <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+</body>
--- admin/imports/nav-admin.php
+++ admin/imports/nav-admin.php
@@ -248,6 +248,13 @@
       </li>
 
       <li class="nav-item">
+        <a class="nav-link " href="../MCQ/dashboard.php">
+          <i class="bi bi-people"></i>
+          <span>MCQ Test</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#client-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-menu-button-wide"></i><span>Client</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
