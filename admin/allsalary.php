e54de616047da2b4af23f0708393086cccfb772d
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2022-07-09 23:06:42 +0530

    Salary View UI Fixes

--- admin/allsalary.php
+++ admin/allsalary.php
@@ -39,6 +39,9 @@
 
 <body>
 
+      <?php
+        include './imports/nav-admin.php';
+      ?>
   <!-- ======= Top and Side Bar ======= -->
   <main id="main" class="main">
 
@@ -65,7 +68,6 @@
               if(!$conn){
                   die("Connection failed: " . mysqli_connect_error());
               }
-              include './imports/nav-admin.php';
 
               /*
               $sql = "SELECT SUM(bsalary) as salary from salaryt";
@@ -264,7 +266,6 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  
   <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="../assets/vendor/chart.js/chart.min.js"></script>
@@ -273,21 +274,9 @@
   <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="../assets/vendor/php-email-form/validate.js"></script>
-
-
-  <script src="../assets/vendor/apexcharts/apexcharts.min.js"></script>
-  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../assets/vendor/chart.js/chart.min.js"></script>
-  <script src="../assets/vendor/echarts/echarts.min.js"></script>
-  <script src="../assets/vendor/quill/quill.min.js"></script>
-  <script src="../assets/vendor/simple-datatables/simple-datatables.js"></script>
-  <script src="../assets/vendor/tinymce/tinymce.min.js"></script>
-  <script src="../assets/vendor/php-email-form/validate.js"></script>
-  <script src="../assets/js/main.js"></script>
 
   <!-- Template Main JS File -->
   <script src="../assets/js/main.js"></script>
-
 </body>
 
 </html>
